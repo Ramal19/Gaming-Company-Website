559cdbb914e0f2a0af8d85e8372db6cf0f57082c
Ramal19 <ld4xz51vd@code.edu.az>
2025-06-01 19:00:11 +0400

    The end

--- script/script.js
+++ script/script.js
@@ -3,9 +3,11 @@ let downloadButton = document.getElementById("download-button");
 let header = document.querySelector("header");
 let topButton = null;
 let cardsSection = document.querySelector(".card-three");
+let menuButton = document.querySelector(".menu-button");
+let isMenuOpen = null;
 
 window.addEventListener('scroll', () => {
-    (window.scrollY > 800) ?  navBar.style.display = "none" : navBar.style.display = "flex";
+    (window.scrollY > 800) ? navBar.style.display = "none" : navBar.style.display = "flex";
 
     if (window.scrollY > 0) {
         if (!topButton) {
@@ -23,9 +25,44 @@ window.addEventListener('scroll', () => {
     }
 
     console.log(window.scrollY, window.innerHeight);
-    
+
 });
 
 downloadButton.addEventListener('click', () => {
     cardsSection.scrollIntoView({ behavior: "smooth" });
 });
+if (menuButton) {
+    menuButton.addEventListener('click', () => {
+        document.body.style.overflow = "hidden";
+        if (!isMenuOpen) {
+            isMenuOpen = document.createElement("div");
+            isMenuOpen.classList.add("menu-open");
+            isMenuOpen.innerHTML = `
+                <button class="close-menu-button"><i class="bi bi-x"></i></button>
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#services">Services</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            `;
+
+            header.appendChild(isMenuOpen);
+            let closeMenuButton = isMenuOpen.querySelector(".close-menu-button");
+            closeMenuButton.addEventListener('click', () => {
+                isMenuOpen.remove();
+                isMenuOpen = null;
+                document.body.style.overflow = "auto";
+            });
+
+            let menuLinks = isMenuOpen.querySelectorAll("a");
+            menuLinks.forEach(link => {
+                link.addEventListener('click', () => {
+                    isMenuOpen.remove();
+                    isMenuOpen = null;
+                    document.body.style.overflow = "auto";
+                })
+            })
+        }
+    })
+}
